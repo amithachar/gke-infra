48ecf894b7e200d1133fa55ebc326708e4e4322c
Amith Acharya <amith99achar@gmail.com>
2026-02-10 13:46:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_container_cluster" "gke" {
   name     = "ott-cluster"
-  location = var.region
+  location = var.zone   # 🔥 Use zone, not region
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -8,13 +8,13 @@ resource "google_container_cluster" "gke" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name     = "ott-node-pool"
-  location = var.region
+  location = var.zone   # 🔥 Zonal
   cluster  = google_container_cluster.gke.name
 
   node_config {
-    machine_type = "e2-micro"   # 🔥 smallest possible
-    disk_type    = "pd-standard"  # 🔥 NOT SSD
-    disk_size_gb = 10            # 🔥 minimal disk
+    machine_type = "e2-micro"
+    disk_type    = "pd-standard"
+    disk_size_gb = 10
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
